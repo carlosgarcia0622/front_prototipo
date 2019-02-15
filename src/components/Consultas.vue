77b7db9ee1b7fbd4599604631b87078f2cc6cae0
Carlos Garcia <carlos.garcia3@udea.edu.co>
2019-02-15 14:38:16 -0500

    reestablecer consultas

--- src/components/Consultas.vue
+++ src/components/Consultas.vue
@@ -21,12 +21,12 @@
             <td>{{response.Record.owner}}</td>
             <td>{{response.Record.state}}</td>
           </tr>
-
+                 
         
           
         </tbody>
       </table>
-     <span >checkedMedicines: {{checkedMedicines}}  
+     <!-- <span >checkedMedicines: {{checkedMedicines}}  </span>
       <span>{{checkedMedicinesString}}</span>-->
       <div>
         <button v-on:click="Search()">Buscar</button>
